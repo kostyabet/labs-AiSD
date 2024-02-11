b581e72b63da6ca52170c2a631dacf5679cc7c76
Kostya <betenya.kostya@mail.ru>
2024-02-11 15:47:09 +0300

    input IO Checks

--- Lab1/Lab1.dpr
+++ Lab1/Lab1.dpr
@@ -4,6 +4,13 @@
 {$R *.res}
 
 Uses
+    Velthuis.Sizes in 'Source/Velthuis.Sizes.pas', 
+    Velthuis.FloatUtils in 'Source/Velthuis.FloatUtils.pas', 
+    Velthuis.StrConsts in 'Source/Velthuis.StrConsts.pas',
+    Velthuis.Numerics in 'Source/Velthuis.Numerics.pas',
+    CompilerAndRTLVersions in 'Source/CompilerAndRTLVersions.pas',
+    Velthuis.RandomNumbers in 'Source/Velthuis.RandomNumbers.pas',
+    Velthuis.BigIntegers In 'Source/Velthuis.BigIntegers.pas',
     System.SysUtils;
 
 Type
@@ -15,14 +22,38 @@ Type
         Next: Pt; { * Next point adress * }
     End;
 
-Function Make(X: Pt; Size: Integer): Elem;
+Function InputNum(Prompt: String; Const MIN, MAX: Integer): Integer;
+Var
+    N: Integer;
+    IsCorrect: Boolean;
+Begin
+    Write(Prompt);
+    N := 0;
+    IsCorrect := False;
+    Repeat
+        Try
+            Read(N);
+
+            IsCorrect := (N >= MIN) And (N <= MAX);
+        Except
+            IsCorrect := False;
+        End;
+        If Not IsCorrect Then
+            Write('Number shell be from ', MIN, ' to ', MAX, '. Try again: ');
+    Until IsCorrect;
+
+    InputNum := N;
+End;
+
+Function Make(X: Pt; Size: Integer; Const MIN, MAX: Integer): Elem;
 Var
     I, Temp: Integer;
+    Prompt: String;
 Begin
     For I := Size DownTo 1 Do
     Begin
-        Write('Write your a' + IntToStr(I) + ': ');
-        Readln(Temp);
+        Prompt := 'Write your a' + IntToStr(I) + ': ';
+        Temp := InputNum(Prompt, MIN, MAX);
         If (Temp <> 0) Then
         Begin
             New(X^.Next);
@@ -41,25 +72,15 @@ Begin
         If X^.Data > 0 Then
         Begin
             Write('+');
-            Write(Format('%.d', [X^.Data]));
+            Write(X^.Data);
         End
         Else
-            Write(Format('(%.d)', [X^.Data]));
+            Write(X^.Data);
 
         Write('*x^', X^.Degree);
         X := X^.Next;
     End;
     Write(#13#10);
-End;
-
-Function InputNum(Prompt: String): Integer;
-Var
-    N: Integer;
-Begin
-    Write(Prompt);
-    Read(N);
-
-    InputNum := N;
 End;
 
 Function Equality(P, Q: Pt): Boolean;
@@ -78,9 +99,9 @@ Begin
     Equality := Not(((P <> Nil) And (Q = Nil)) Or ((P = Nil) And (Q <> Nil)));
 End;
 
-Function Meaning(P: Pt; X: Integer): Integer;
+Function Meaning(P: Pt; X: Integer): BigInteger;
 Var
-    Res, InterimRes: Integer;
+    Res, InterimRes: BigInteger;
     I: Integer;
 Begin
     Res := 0;
@@ -145,24 +166,31 @@ Begin
     End;
 End;
 
+Const
+    MIN_SIZE: Integer = 1;
+    MAX_SIZE: Integer = 50;
+    MIN_X: Integer = -100_000_000;
+    MAX_X: Integer = 100_000_000;
+
 Var
     P1, P2, P3: Pt; { * Pointer on first element of list * }
     Size: Integer; { * Number of elements * }
-    X, P1Result, P2Result: Integer;
+    X:Integer;
+    P1Result, P2Result: BigInteger;
 
     //AiSD Lab 1
 Begin
     Writeln('Laboratory 1, Variant 1:');
 
-    Size := InputNum(#13#10'Input number of P1 elements: ');
+    Size := InputNum(#13#10'Input number of P1 elements: ', MIN_SIZE, MAX_SIZE);
     New(P1);
     Writeln('Input P1.');
-    Make(P1, Size);
+    Make(P1, Size, MIN_X, MAX_X);
 
-    Size := InputNum(#13#10'Input number of P2 elements: ');
+    Size := InputNum(#13#10'Input number of P2 elements: ', MIN_SIZE, MAX_SIZE);
     New(P2);
     Writeln('Input P2.');
-    Make(P2, Size);
+    Make(P2, Size, MIN_X, MAX_X);
 
     Write(#13#10'P1: ');
     Print(P1^.Next);
@@ -172,13 +200,13 @@ Begin
 
     Writeln('Equality of P1 And P2: ', Equality(P1^.Next, P2^.Next));
 
-    X := InputNum(#13#10'Input X-Pointer for P1: ');
+    X := InputNum(#13#10'Input X-Pointer for P1: ', MIN_X, MAX_X);
     P1Result := Meaning(P1^.Next, X);
-    Writeln('At X=', X, ' the result is: ', P1Result);
+    Writeln('At X=', X, ' the result is: ', P1Result.ToString);
 
-    X := InputNum(#13#10'Input X-Pointer for P1: ');
+    X := InputNum(#13#10'Input X-Pointer for P1: ', MIN_X, MAX_X);
     P2Result := Meaning(P2^.Next, X);
-    Writeln('At X=', X, ' the result is: ', P2Result);
+    Writeln('At X=', X, ' the result is: ', P2Result.ToString);
 
     New(P3);
     Add(P1^.Next, P2^.Next, P3);
